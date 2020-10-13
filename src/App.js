c94b83560459679f8e238248466e4ad5054299c4
Thomas Brewer <tcb93@rutgers.edu>
2020-10-12 20:50:33 -0400

    initialized firebase app and environment variables

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
+import firebase from 'firebase/app';
 
-function App() {
+const App = () => {
+  // load in environment variables
+  require('dotenv').config();
+
+  // connect to firebase services
+  firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASURE_ID
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello world!</h1>
       </header>
     </div>
   );
