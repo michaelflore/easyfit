33342043455f335e13fbf76d63dbbe13df6ea4cb
Thomas Brewer <tcb93@rutgers.edu>
2020-10-15 13:38:09 -0400

    integrated calculation logic into UI

--- src/BMICalculator.js
+++ src/BMICalculator.js
@@ -24,32 +24,25 @@ const BMICalculator = () => {
   };
 
   // parses new value for weight/height and switches state to reflect new value
-  const weightChange = (event) => {
-    try {
-      let newWeight = parseFloat(event.target.value);
-      setWeight(newWeight);
-    } catch(err) {
-      alert("Weight must be a valid decimal value.");
-    }
-  };
-  const heightChange = (event) => {
-    try {
-      let newHeight = parseFloat(event.target.value);
-      setHeight(newHeight);
-    } catch(err) {
-      alert("Height must be a valid decimal value.");
-    }
-  };
+  const weightChange = (event) => setWeight(parseFloat(event.target.value));
+  const heightChange = (event) => setHeight(parseFloat(event.target.value));
 
-  // TODO: implement this
+  // BMI is calculated here and advice is given
   const calculateBMI = (event) => {
-    alert("This has not been implemented yet!");
+    // validate input, and calculate bmi
+    if(!Number.isInteger(weight) || !Number.isInteger(height)) {
+      alert("Error, use must enter a valid weight and height.");
+    } else if(height <= 0) {
+      alert("Error, height must be a positive, non-zero number.")
+    } else {
+      setBMI(unit === 0 ? bmiMetric(height, weight) : bmiImperial(height, weight));
+    }
   };
   
   /**
    * This looks scary, I promise it isn't.
    * 
-   * We load in our custom Header app bar first, and then surround our entire page in a Paper object for styling.
+   * We load in our custom Header app bar first
    * Our calculator form is aligned and structured in a Grid, with one container Grid tag and many item Grid tags.
    * TextField tags are objects that take in text input. We have one for weight and one for height, and their state changes on input.
    * RadioGroup simply manages our radio buttons for us.
@@ -63,10 +56,10 @@ const BMICalculator = () => {
         <form noValidate autoComplete="off">
           <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
             <Grid item>
-              <TextField id="bmi-weight" label={`Enter Your Weight (${unit === 0 ? "kg" : "lbs"})`} defaultValue={weight.toString()} variant="outlined" onInput={weightChange}/>
+              <TextField id="bmi-weight" label={`Enter Your Weight (${unit === 0 ? "kg" : "lbs"})`} defaultValue="0" variant="outlined" onInput={weightChange}/>
             </Grid>
             <Grid item>
-              <TextField id="bmi-height" label={`Enter Your Height (${unit === 0 ? "m" : "in"})`} defaultValue={height.toString()} variant="outlined" onInput={heightChange}/>
+              <TextField id="bmi-height" label={`Enter Your Height (${unit === 0 ? "m" : "in"})`} defaultValue="0" variant="outlined" onInput={heightChange}/>
             </Grid>
             <Grid item>
               <FormLabel>Select Units</FormLabel>
