c47dc637ab81711c91e3a2c862351fa471a3c3ca
Thomas Brewer <tcb93@rutgers.edu>
2020-10-15 13:01:31 -0400

    fixed bug with app bar hiding elements at the top of the page

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    offset: theme.mixins.toolbar
+}));
 
 const Header = (props) => {
     // acquires title for the current feature from the jsx props
     const barTitle = props.title;
 
+    const styles = useStyles();
+
     // returns app bar at the top of the page that will be used for navigation between features
     return (
-    <AppBar position="static" color="primary">
-        <Toolbar>
-            <Typography align="center">{barTitle}</Typography>
-        </Toolbar>
-    </AppBar>
+    <Fragment>
+        <AppBar position="fixed" color="primary">
+            <Toolbar>
+                <Typography align="center">{barTitle}</Typography>
+            </Toolbar>
+        </AppBar>
+        <div className={styles.offset} />
+    </Fragment>
     );
 };
 
