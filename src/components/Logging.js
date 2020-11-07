0a117672421af02212c2b2c95dfbcb1e7bcc280f
Thomas Brewer <tcb93@rutgers.edu>
2020-11-07 13:50:11 -0500

    Implementing firestore weight/bmi logging logic

--- src/components/Logging.js
+++ src/components/Logging.js
@@ -2,21 +2,49 @@ import React, { Fragment, useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import Header from './Header';
 import { Card, Grid, Typography, TextField, Button } from '@material-ui/core';
+import firebase from "firebase/app";
+import 'firebase/firestore';
 
 const Logging = () => {
     const [weight, setWeight] = useState(0);
     const [bmi, setBMI] = useState(0);
+    const uid = firebase.auth().currentUser.uid;
+    const db = firebase.firestore();
 
     // debug
     useEffect(() => {
         console.log(weight);
         console.log(bmi);
-    }, [weight, bmi])
+    }, [weight, bmi]);
+
+    const isFloat = n => {
+        return Number(n) === n && n % 1 !== 0;
+    }
+
+    const validate = () => {
+        if(!(isFloat(weight) || Number.isInteger(weight)) || !(isFloat(bmi) || Number.isInteger(bmi))) {
+            alert("Error, use must enter a valid weight and bmi.");
+            return false;
+        } else {
+            return true;
+        }
+    };
 
     const handleSubmit = e => {
         e.preventDefault();
-        // TODO actual logging logic here
-        alert('log successful!');
+        // validation
+        if(validate()) {
+            let timestamp = Date.now();
+            console.log(timestamp);
+            // logging
+            db.collection('users').doc(uid).collection('logs').add({
+                weight: weight,
+                bmi: bmi,
+                date: firebase.firestore.Timestamp.fromDate(new Date())
+            })
+            .then(() => console.log('log successful!'));
+        }
+        alert('end of function!');
     };
 
     return (
@@ -28,10 +56,10 @@ const Logging = () => {
                     <br/>
                     <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
                         <Grid item>
-                            <TextField id="weight-in" label='Enter your new weight (lbs)' defaultValue={weight} variant="outlined" onInput={e => setWeight(e.target.value)}/>
+                            <TextField id="weight-in" label='Enter your new weight (lbs)' defaultValue={0} variant="outlined" onInput={e => setWeight(parseFloat(e.target.value))}/>
                         </Grid>
                         <Grid item>
-                            <TextField id="bmi-in" label='Enter your new BMI' defaultValue={bmi} variant="outlined" onInput={e => setBMI(e.target.value)}/>
+                            <TextField id="bmi-in" label='Enter your new BMI' defaultValue={0} variant="outlined" onInput={e => setBMI(parseFloat(e.target.value))}/>
                         </Grid>
                         <Grid item>
                             <Button type='submit' variant="contained" color="primary">Log</Button>
