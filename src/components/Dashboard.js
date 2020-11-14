5341f5552193b0d11c43a97e8e685af79c3eecb9
Thomas Brewer <tcb93@rutgers.edu>
2020-11-14 16:37:08 -0500

    Added navbar to admin dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState, useEffect} from "react";
 import Header from "./Header";
 import { Card, Grid, Typography } from "@material-ui/core";
+import NavBar from './NavBar';
 import firebase from "firebase";
 
 //Table
@@ -32,6 +33,7 @@ const Dashboard = () => {
     return (
         <Fragment>
             <Header title="Admin Dashboard"/>
+            <NavBar/>
             <Card>
                 <Grid container direction="column" justify="center" alignItems="center" spacing={1}>
                     <Table>
