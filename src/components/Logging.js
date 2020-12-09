3c163bacc9cdfa0f35de3ec263caf7e116b01b49
Thomas Brewer <tcb93@rutgers.edu>
2020-12-09 17:05:48 -0500

    Changed activity level input to dropdown menu

--- src/components/Logging.js
+++ src/components/Logging.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import NavBar from './NavBar';
 import Header from './Header';
-import { Card, Grid, TextField, Button, makeStyles } from '@material-ui/core';
+import { Card, Grid, TextField, Button, makeStyles, Select, MenuItem } from '@material-ui/core';
 import firebase from "firebase/app";
 import 'firebase/firestore';
 import { logWeight } from '../logic/logginglogic';
@@ -14,17 +14,16 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Logging = () => {
+    let activityLevels = [1, 2, 3, 4];
     // initial state for weight and bmi
     const [weight, setWeight] = useState(0);
     const [bmi, setBMI] = useState(0);
-    const [alevel, setALevel] = useState(-1);
+    const [alevel, setALevel] = useState(activityLevels[0]);
     // current user id
     const uid = firebase.auth().currentUser.uid;
     // database connection
     const db = firebase.firestore();
     const styles = useStyles();
-    const MIN_ALEVEL = 1;
-    const MAX_ALEVEL = 4;
 
     // // on form submit, the current data of the page is validated and then logged to the current user document
     const handleSubmit = e => {
@@ -48,7 +47,11 @@ const Logging = () => {
                             <TextField id="bmi-in" label='Enter your new BMI' defaultValue={0} variant="outlined" onInput={e => setBMI(parseFloat(e.target.value))}/>
                         </Grid>
                         <Grid item>
-                            <TextField id='activity' label={`Activity Level (${MIN_ALEVEL.toString()}-${MAX_ALEVEL.toString()})`} variant='outlined' onInput={e => setALevel(Number.parseInt(e.target.value))}/>
+                            <Select onChange={e => setALevel(Number.parseInt(e.target.value))} value={alevel}>
+                                {activityLevels.map(level => {
+                                    return <MenuItem value={level} key={level}>{level}</MenuItem>
+                                })}
+                            </Select>
                         </Grid>
                         <Grid item>
                             <Button type='submit' variant="contained" color="primary">Log</Button>
